a115425932db4f974e8b16c40a35a30a0a7a37e6
bharath-pranav <bharath.pranav@surveysparrow.com>
2025-10-30 16:51:12 +0530

    Upload 47 test cases from Asura.ai (#28)

---
+++ test/frontend/assignee-performance/verify-back-navigation-from-detail-page.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test.setTimeout(300000); // 5 minutes timeout
+
+test.use({
+  storageState: '/Users/Kabeleswar.pe/Library/Application Support/aitest-case-generator/storage/storageState-project-1761294702676.json'
+});
+
+test('test', async ({ page }) => {
+  await page.goto('https://dev-perf-frontend.vercel.app/?teams=AI%2Csparrow-crm%2Csparrowdesk%2Csparrowgenie%2Css-analyze%2Css-appnest%2Css-core%2Css-deals-mobile%2Css-devops%2Css-hygiene-cloud%2Css-integrations%2Css-plgdataops%2Css-reputation%2Css-sre%2Css-website%2Cts-core%2Cts-engage%2Cts-goal%2Cts-performance');
+  await page.getByRole('button', { name: 'Story Points' }).click();
+  await page.locator('div').filter({ hasText: /^Aakash S$/ }).nth(1).click();
+  await page.getByRole('button').filter({ hasText: /^$/ }).click();
+  await expect(page.getByRole('main')).toContainText('Assignees Performance');
+});
---
+++ test/frontend/issues/verify-time-range-filter.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test.setTimeout(300000); // 5 minutes timeout
+
+test.use({
+  storageState: '/Users/Kabeleswar.pe/Library/Application Support/aitest-case-generator/storage/storageState-project-1761294702676.json'
+});
+
+test('test', async ({ page }) => {
+  await page.goto('https://dev-perf-frontend.vercel.app/?teams=AI%2Csparrow-crm%2Csparrowdesk%2Csparrowgenie%2Css-analyze%2Css-appnest%2Css-core%2Css-deals-mobile%2Css-devops%2Css-hygiene-cloud%2Css-integrations%2Css-plgdataops%2Css-reputation%2Css-sre%2Css-website%2Cts-core%2Cts-engage%2Cts-goal%2Cts-performance');
+  await page.getByRole('button', { name: 'Issues' }).click();
+  await expect(page.locator('main')).toContainText('Today');
+  await page.getByRole('button', { name: 'Today' }).click();
+  await page.getByText('Past 7 days').click();
+  await expect(page.locator('main')).toContainText('Past 7 days');
+  await expect(page.getByRole('main')).toContainText('In Progress - PR Created');
+  await expect(page.getByRole('main')).toContainText('In Progress - PR Created');
+  await expect(page.getByRole('main')).toContainText('In Progress - Branch Created');
+});
