0dbf5323fe5a64f05b100259d6b27cdd5eb1875b
bharath-pranav <bharath.pranav@surveysparrow.com>
2025-10-30 16:50:36 +0530

    Upload 47 test cases from Asura.ai

---
+++ test/frontend/jira-metrics/verify-squads-filter-select-single-squad.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test.setTimeout(300000); // 5 minutes timeout
+
+test.use({
+  storageState: '/Users/Kabeleswar.pe/Library/Application Support/aitest-case-generator/storage/storageState-project-1761294702676.json'
+});
+
+test('test', async ({ page }) => {
+  await page.goto('https://dev-perf-frontend.vercel.app/?teams=AI%2Csparrow-crm%2Csparrowdesk%2Csparrowgenie%2Css-analyze%2Css-appnest%2Css-core%2Css-deals-mobile%2Css-devops%2Css-hygiene-cloud%2Css-integrations%2Css-plgdataops%2Css-reputation%2Css-sre%2Css-website%2Cts-core%2Cts-engage%2Cts-goal%2Cts-performance');
+  await page.getByRole('button', { name: 'DORA Metrics' }).click();
+  await page.getByRole('button', { name: 'Squads' }).click();
+  await page.getByRole('button', { name: 'Clear' }).click();
+  await page.locator('div').filter({ hasText: /^Anchor$/ }).first().click();
+  await page.locator('.fixed.inset-0').click();
+  await expect(page.getByRole('main')).toContainText('Squads1');
+  await expect(page.getByRole('main')).toContainText('DORA Metrics');
+  await expect(page.getByRole('main')).toContainText('Cycle Time Metrics : Story Point');
+});
